c7c3f4dd9fbdcfea374a37e69a5c6042c8ce0957
Frederico Bittencourt <frederico.bittencourt01@sap.com>
2018-06-04 14:28:19 -0300

    import repository
    fix install and nbin path in nv script
    
    implement Reading from /home/I850193/.config/nv/node_versions.config
    
    (+)10.3.0 (latest)
    (+)8.11.2 (lts)*
    
    ( ) -> Not Downloaded
    (+) -> Downloaded
    * selected
    
    use list remote versions
    
    show version names when listing from remote
    
    use remote to get latest version when downloading
    
    remove useless variable

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
-NV_CONFIG=$HOME/.config/nv
-NV_VERSIONS=$NV_CONFIG/node_versions.config
+#!/bin/bash
 
-NV_REPO="https:some"
+NV_CONFIG=$HOME/.config/nv
+
+NV_REPO="https://fredericorb@bitbucket.org/fredericorb/nv.git"
 
 GIT_CLONE_FLAGS="--depth 1"
 
 install () {
+	echo "Installing Node Versioning"
 	git=$(which git)
 	if [ $? != 0 ]; then
 		echo "Could not found git.\nPlease install git before installing Node Versioning"
@@ -17,10 +19,12 @@ install () {
 		exit 1
 	fi
 	$git clone $GIT_CLONE_FLAGS $NV_REPO $NV_CONFIG
-	mkdir -p $NV_REPO/bin
-	mkdir -p $NV_REPO/nbin
-	ln -s $NV_REPO/nv.sh $NV_REPO/bin/nv
-	echo "export PATH=$PATH:$HOME/.config/nv/bin:$HOME/.config/nv/nbin" >> $HOME/.profile
+	echo "Setting up bin and nbin folders"
+	mkdir -p $NV_CONFIG/bin
+	ln -s $NV_CONFIG/nv.sh $NV_CONFIG/bin/nv
+	add_path="export PATH=\"\$PATH:$HOME/.config/nv/bin:$HOME/.config/nv/nbin\""
+	echo "Adding $add_path to $HOME/.profile"
+	echo  $add_path >> $HOME/.profile
 }
 
 install
--- nv.sh
+++ nv.sh
@@ -1,29 +1,95 @@
+#!/bin/bash
+
 CONFIG_FOLDER=$HOME/.config/nv
-CONFIG_FILE=$CONFIG_FOLDER/node_versions.config
-BIN_LINK=$CONFIG_FOLDER/bin
+BIN_LINK=$CONFIG_FOLDER/nbin
+NODE_VERSION_REGEX='s/\([0-9]*\.[0-9]*\.[0-9]\).*/\1/p'
+
+NODE_LATEST_DIST_URL="https://nodejs.org/dist/latest/"
+NODE_CARBON_DIST_URL="https://nodejs.org/dist/latest-carbon/"
+NODE_BORON_DIST_URL="https://nodejs.org/dist/latest-boron/"
+NODE_ARGON_DIST_URL="https://nodejs.org/dist/latest-argon/"
+
+REMOTE_URLS=($NODE_LATEST_DIST_URL $NODE_CARBON_DIST_URL $NODE_BORON_DIST_URL $NODE_ARGON_DIST_URL)
+REMOTE_NAME=("latest" "carbon" "boron" "argon")
 
 wget=$(which wget)
 
+net_get_latest_version () {
+	$wget -qO- $1 | grep node-v | sed 's/<a href="node-v//' | head -1 | sed -n $NODE_VERSION_REGEX
+}
+
+_list_remote () {
+	echo "Getting remote versions"
+	echo "This might take a while..."
+	echo
+	
+	v=0
+	for url in ${REMOTE_URLS[@]}; do
+		version=$(net_get_latest_version $url)	
+		if [ -d "$CONFIG_FOLDER/node-v$version-linux-x64" ]; then
+			echo "  [x] $version (${REMOTE_NAME[$v]})"
+		else
+			echo "  [ ] $version (${REMOTE_NAME[$v]})"
+		fi
+		v=$((v + 1))
+	done
+
+	echo 
+	echo "[x] -> Downloaded"
+	echo "[ ] -> Not downloaded"
+}
+
+_list_local () {
+	echo "Getting installed versions"
+	for i in $(ls $CONFIG_FOLDER | grep node-v | sed 's/node-v//' | sed -n $NODE_VERSION_REGEX); do
+		if [ -f $HOME/.nversion ]; then
+			selected=$(cat $HOME/.nversion)
+		else
+			selected="0.0"
+		fi
+		if [ "$selected" = "$i" ]; then
+			echo "  *$i"
+		else
+			echo "   $i"
+		fi
+	done
+	echo
+	echo "* -> selected"
+}
+
 list_cmd () {
-	echo "Reading from $CONFIG_FILE"
-	cat $CONFIG_FILE
+	if [ ! -z $1 ]; then
+		case $1 in 
+			-r|--remote)
+				_list_remote
+				;;
+			*)
+				echo "Unknown parameter $1 for list command"
+				exit 1
+		esac
+	else
+		_list_local
+	fi
 }
 
 get_cmd () {
 	version=$1
 	shift
 	if [ -z $version ]; then
-		version=$(cat $CONFIG_FILE | grep latest | gawk '{ print $1 }')
+		echo "Geting latest version from remote"
+		version=$(net_get_latest_version $NODE_LATEST_DIST_URL)
+		echo "Downloading version $version"
 	fi
 	filename="node-v$version-linux-x64.tar.xz"
 	url="https://nodejs.org/dist/v$version/$filename"	
 	folder="$CONFIG_FOLDER/node-v$version-linux-x64"
 	if [ -d $folder ]; then
 		echo "Version $version already installed"	
-		echo "\nTo use this version, you can type \`nv use $version\`"
-		echo "\t\`nv use $version\`"
+		echo
+		echo "To use this version, you can type \`nv use $version\`"
+		echo "  \`nv use $version\`"
 		echo "Or simply:"
-		echo "\t\`nv use\`"
+		echo "  \`nv use\`"
 		echo "if you'd like to use the latest vesrsion"
 		exit 1
 	fi
@@ -46,16 +112,13 @@ get_cmd () {
 use_cmd () {
 	version=$1
 	if [ -z $version ]; then
-		echo "Getting latest version..."
-		version=$(cat $CONFIG_FILE | grep latest | gawk '{ print $1 }')
+		echo "Please specify a version"
+		echo "usage: nv use <version>"
+		echo
+		exit 1
 	fi
 	echo "Using node $version"
-	local NODE_VERSION=$version
-	if [ -f $HOME/.nversion ]; then
-		rm $HOME/.nversion
-	fi
-	echo $version >> $HOME/.nversion
-	folder="$CONFIG_FOLDER/node-v$version-linux-x64/bin"
+		folder="$CONFIG_FOLDER/node-v$version-linux-x64/bin"
 	if [ ! -d $folder ]; then
 		echo "Could not find folder $folder\n"
 		echo "Invalid version $version of nodejs"
@@ -63,6 +126,11 @@ use_cmd () {
 		exit 1
 	else	
 		echo "Node folder set to $folder" 
+		local NODE_VERSION=$version
+		if [ -f $HOME/.nversion ]; then
+			rm $HOME/.nversion
+		fi
+		echo $version >> $HOME/.nversion
 	fi
 	if [ -d $BIN_LINK ]; then
 		rm $BIN_LINK
